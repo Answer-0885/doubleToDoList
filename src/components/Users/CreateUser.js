1070b3fa39153cd54d63eab421c9c041d021cad1
Vitaly <answer-0885@mail.ru>
2022-11-30 16:54:28 +0700

    Лекция 84 1-часть

--- src/components/Users/CreateUser.js
+++ src/components/Users/CreateUser.js
@@ -1,21 +1,33 @@
-import Card from "../UI/Card"
-import styles from "./CreateUser.module.css"
-import Button from "../UI/Button"
 import React, {useState} from "react"
+import Card from "../UI/Card"
+import Button from "../UI/Button"
+
+import styles from "./CreateUser.module.css"
+import ErrorModal from "../UI/ErrorModal"
+
 
 const CreateUser = (props) => {
 
     const[inputName, setInputName] = useState(''); // где inputName - переменная состояния, а setInputName - функция.
     const[inputAge, setInputAge] = useState(''); // где inputAge - переменная состояния, а setInputAge - функция.
+    const[error, setError] = useState(); // где error - переменная состояния, а setError - функция.
 
-    const CreateUserHandler = (event) => {// Создаёт нового пользователя на основе введённых данных в поля.
+    const createUserHandler = (event) => {// Создаёт нового пользователя на основе введённых данных в поля.
            event.preventDefault();
            // Проверяем чтобы инпуты не были пустыми
            if(inputName.trim().length === 0 || inputAge.trim().length === 0){
+            setError({
+                title:'Некорректный ввод',
+                message:'Эти поля не могут быть пустыми'
+            })
             return;
            } 
            // Прверяем чтобы возраст не был меньше значения единицы
            if(+inputAge < 1){
+            setError({
+                title:'Некорректный возраст',
+                message:'Возраст должен быть больше 0'
+            })
             return;
            }
            // Стираем значения из переменных inputName и inputName в момент нажатия на кнопку Button;
@@ -29,16 +41,40 @@ const CreateUser = (props) => {
     const ageChangeHandler = (event) => {// Эта функция отслеживает инпут age и записывает его значение в переменную inputAge
         setInputAge(event.target.value);
     }
-    return (
-        <Card className={styles.input}>
-            <form onSubmit={CreateUserHandler}>
-                <label htmlFor="name">Имя</label>
-                <input id="name" type='text' onChange={nameChangeHandler} value={inputName}/>
-                <label htmlFor="age">Возраст</label>
-                <input id="age" type='number' onChange={ageChangeHandler} value={inputAge}/>
-                <Button type="submit">Добавить пользователя</Button>
-            </form>
-        </Card>
-    )
+
+    const errorHandler = () => {
+        setError(false)
+    }
+
+   return (
+    <div>
+      {error && (
+        <ErrorModal
+          onCloseModal={errorHandler}
+          title={error.title}
+          message={error.message}
+        />
+      )}
+      <Card className={styles.input}>
+        <form onSubmit={createUserHandler}>
+          <label htmlFor="name">Имя</label>
+          <input
+            id="name"
+            type="text"
+            onChange={nameChangeHandler}
+            value={inputName}
+          />
+          <label htmlFor="age">Возраст</label>
+          <input
+            id="age"
+            type="number"
+            onChange={ageChangeHandler}
+            value={inputAge}
+          />
+          <Button type="submit">Добавить Пользователя</Button>
+        </form>
+      </Card>
+    </div>
+  );
 }
 export default CreateUser;
